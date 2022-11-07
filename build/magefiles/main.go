4e1c1e6d51e5e1b04c0d28ef9003a2f579e6ef58
David Jackson <davidjackson@Davids-MacBook-Pro.local>
2022-11-06 16:18:19 -0800

    docker build

--- build/magefiles/main.go
+++ build/magefiles/main.go
@@ -12,11 +12,19 @@ import (
 	"github.com/davidwallacejackson/dagger-monorepo-dep-crawler/build/dep-scanner/core"
 )
 
+func getOsArch() (string, string) {
+	return runtime.GOOS, runtime.GOARCH
+}
+
+func getWorkdir(client *dagger.Client) *dagger.Directory {
+	return client.Host().Workdir(dagger.HostWorkdirOpts{
+		Exclude: []string{"dist", "projects/frontend/node_modules"},
+	})
+}
+
 func apiBinary(ctx context.Context, client *dagger.Client, goos string, goarch string) (*dagger.File, error) {
 
-	dir := client.Host().Workdir(dagger.HostWorkdirOpts{
-		Exclude: []string{"dist"},
-	})
+	dir := getWorkdir(client)
 
 	dependencyScanner := depscanner.NewDependencyScanner(client, dir)
 
@@ -61,11 +69,13 @@ func APIDev(ctx context.Context) error {
 	return err
 }
 
-func frontendBuild(ctx context.Context, client *dagger.Client) (*dagger.Directory, error) {
-	dir := client.Host().Workdir(dagger.HostWorkdirOpts{
-		Exclude: []string{"dist", "projects/frontend/node_modules"},
-	})
+func apiProd(ctx context.Context, client *dagger.Client) (*dagger.File, error) {
+	_, goarch := getOsArch()
+	return apiBinary(ctx, client, "linux", goarch)
+}
 
+func frontendBuild(ctx context.Context, client *dagger.Client) (*dagger.Directory, error) {
+	dir := getWorkdir(client)
 	dependencyScanner := depscanner.NewDependencyScanner(client, dir)
 
 	sparseDir, err := dependencyScanner.GetSubdirWithDependencies(ctx, "projects/frontend")
@@ -107,6 +117,31 @@ func Frontend(ctx context.Context) error {
 	return err
 }
 
-func getOsArch() (string, string) {
-	return runtime.GOOS, runtime.GOARCH
+func DockerContainer(ctx context.Context) error {
+	client, err := dagger.Connect(ctx, dagger.WithLogOutput(os.Stdout))
+	if err != nil {
+		return err
+	}
+
+	defer client.Close()
+
+	api, err := apiProd(ctx, client)
+	if err != nil {
+		return err
+	}
+
+	frontend, err := frontendBuild(ctx, client)
+	if err != nil {
+		return err
+	}
+
+	withApi := core.ContainerWithFile(client.Container().From("alpine:latest"), "/app/api", api)
+	withFrontend := core.ContainerWithDirectory(withApi, "/app/static", frontend)
+
+	final := withFrontend.
+		WithWorkdir("/app").
+		WithEntrypoint([]string{"/app/api"})
+
+	_, err = final.Export(ctx, "./dist/container.tgz")
+	return err
 }
